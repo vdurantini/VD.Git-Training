b38d2df2fbb72fe42b898737a91aa328b39f34d6
Vanni Durantini <vannivd@yahoo.com>
2019-12-10 12:15:30 +0100

    Creata lista della spesa

---
+++ src/base/HelloWorld.java
@@ -0,0 +1,10 @@
+package base;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hello World");
+	}
+}
